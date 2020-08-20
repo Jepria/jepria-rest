5ea4dfd7aa626d1494e8a618aaeb144df64ff08d
remitot <9774730@gmail.com>
2020-08-20 09:52:08 +0300

    feat: Rest: поддержка определения формата дат параметров jaxrs-методов

---
+++ src/main/java/org/jepria/server/service/rest/jersey/DateParamConverter.java
@@ -0,0 +1,44 @@
+package org.jepria.server.service.rest.jersey;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ParamConverter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateParamConverter implements ParamConverter<Date> {
+
+  protected DateFormat customDateFormat;
+
+  public void setCustomDateFormat(DateFormat customDateFormat) {
+    this.customDateFormat = customDateFormat;
+  }
+
+  @Override
+  public Date fromString(String s) {
+
+    final String format;
+
+    if (customDateFormat != null) {
+      format = customDateFormat.value();
+    } else {
+      format = DateFormat.DEFAULT_FORMAT;
+    }
+
+    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+
+    try {
+      return simpleDateFormat.parse(s);
+    } catch (ParseException e) {
+      throw new BadRequestException(Response.status(Response.Status.BAD_REQUEST.getStatusCode(),
+              "Incorrect date or date-time format: expected [" + format + "], actual [" + s + "]").build(),
+              e);
+    }
+  }
+  @Override
+  public String toString(Date date) {
+    // TODO why to invoke this?
+    throw new UnsupportedOperationException();
+  }
+}
