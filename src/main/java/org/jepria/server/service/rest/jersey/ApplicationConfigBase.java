c3c5af1f5acfb255e11afc03637bfd07dbbe3626
AlexSav94 <aleksoider@gmail.com>
2020-12-17 13:13:36 +0300

    feat: registerPropertyPathResolver

--- src/main/java/org/jepria/server/service/rest/jersey/ApplicationConfigBase.java
+++ src/main/java/org/jepria/server/service/rest/jersey/ApplicationConfigBase.java
@@ -69,6 +69,8 @@ public class ApplicationConfigBase extends ResourceConfig {
     registerOAuthDynamicFeature();
 
     registerProtectedDynamicFeature();
+  
+    registerPropertyPathResolver();
   }
   
   /**
